5b7a23f4c65946e44caef13834247a27bdae6ce5
Передереев Илья Николаевич <IPeredereev@t1-team.ru>
2026-05-09 16:09:23 +0300

    Создать файл для деталей профиля пользователя

---
+++ src/main/java/com/peredereevin/authservice/service/AppUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.peredereevin.authservice.service;
+
+import com.peredereevin.authservice.entity.User;
+import com.peredereevin.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Почта, которую вы указали, не найдена: " + email));
+        return new org.springframework.security.core.userdetails.User(existingUser.getEmail(), existingUser.getPassword(), new ArrayList<>());
+    }
+}
