742dd10e777f835e57db628f86aaf6a76c67a935
Передереев Илья Николаевич <IPeredereev@t1-team.ru>
2026-05-09 06:22:34 +0300

    Создать контроллер для rest операций с профилем

---
+++ src/main/java/com/peredereevin/authservice/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package com.peredereevin.authservice.controller;
+
+import com.peredereevin.authservice.io.ProfileRequest;
+import com.peredereevin.authservice.io.ProfileResponse;
+import com.peredereevin.authservice.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProfileResponse register(@RequestBody ProfileRequest request){
+        ProfileResponse response = profileService.createProfile(request);
+        //TODO: send email!!!
+        return response;
+    }
+}
