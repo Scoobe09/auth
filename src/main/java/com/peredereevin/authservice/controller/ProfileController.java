98f6eff9108cd81e0edac0a2667f7a520f9cb2a3
Передереев Илья Николаевич <IPeredereev@t1-team.ru>
2026-05-09 08:02:07 +0300

    Добавить проверки при регистрации

--- src/main/java/com/peredereevin/authservice/controller/ProfileController.java
+++ src/main/java/com/peredereevin/authservice/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.peredereevin.authservice.controller;
 import com.peredereevin.authservice.io.ProfileRequest;
 import com.peredereevin.authservice.io.ProfileResponse;
 import com.peredereevin.authservice.service.ProfileService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class ProfileController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public ProfileResponse register(@RequestBody ProfileRequest request){
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request){
         ProfileResponse response = profileService.createProfile(request);
         //TODO: send email!!!
         return response;
--- src/main/java/com/peredereevin/authservice/io/ProfileRequest.java
+++ src/main/java/com/peredereevin/authservice/io/ProfileRequest.java
@@ -1,12 +1,25 @@
 package com.peredereevin.authservice.io;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProfileRequest {
+
+    @NotBlank(message = "Поле name не должно быть пустым")
     private String name;
+    @Email(message = "Введите валидный адрес электронной почты")
+    @NotNull(message = "Поле email не должно быть пустым")
     private String email;
+    @Min(value = 6, message = "Пароль должен содержать минимум 6 символов")
     private String password;
 }
